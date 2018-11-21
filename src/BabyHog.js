6172d074f3103dff83163d63c76e97fc0ff732b5
scotidodson <scotidodson@gmail.com>
2018-11-21 15:55:45 -0500

    Done.

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -5,13 +5,24 @@ import SunBaby from './assets/sun-eyes.png'
 import BlueBaby from './assets/blue-eyes.png'
 import GlowingBaby from './assets/glowing-eyes.png'
 
-// is there a way we could associate eye color string values with images? 
-// perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
+// is there a way we could associate eye color string values with images?
+// perhaps so we couldx do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
+const eyeColorMapper={
+  'blue':BlueBaby,
+  'sun':SunBaby,
+  "glowing":GlowingBaby
+}
 
 export default class BabyHog extends Component {
 
   constructor(props) {
+
+    console.log("in  the  baby constructor");
     super(props)
+    console.log(this.props);
+    this.state = {
+      imgSrc: eyeColorMapper[this.props.eyeColor]
+    }
   }
 
   changeWeight = (e) => {
@@ -21,15 +32,28 @@ export default class BabyHog extends Component {
       weight: newWeight
     })
   }
+    setImage = ()=>{
+      switch(this.props.eyeColor){
+        case "blue":
+          return BlueBaby
+        case "glowing":
+          return GlowingBaby
+        case "sun":
+          return SunBaby
+        default:
+          return normalBaby
+      }
+    }
+
 
   render() {
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
-          
+        <h1>Name: {this.props.name}</h1>
+        <h3>Weight: </h3>
+        <h3>Hobby: {this.props.hobby}</h3>
+        <h4>Eye Color: {this.props.eyeColor}</h4>
+
         <Button name="+">
           Increase Weight
         </Button>
@@ -38,9 +62,9 @@ export default class BabyHog extends Component {
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={eyeColorMapper[this.props.eyeColor]} style={{height: '200px'}} alt="MasterBlasterJrJr" />
         </div>
-        
+
       </li>
     )
   }
--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
   constructor() {
+    console.log("in  the  master constructor");
     super()
     this.state = {
       eyeColor: "blue",
+    //  curId:0
     }
   }
-
 
   changeEyeColor = (e) => {
     this.setState({
@@ -19,10 +20,15 @@ export default class MasterHog extends Component {
     })
   }
 
+  // setCurId = ()=>{
+  //   this.setState({
+  //     curId: this.curId +1
+  //   })
+  // }
 
   render() {
     return (
-      <div>
+      <div >
 
         <input type="radio" name="eyeColor" value="blue" onChange={this.changeEyeColor} />
         Blue<br></br>
@@ -30,19 +36,19 @@ export default class MasterHog extends Component {
         Sun<br></br>
         <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
         Glowing<br></br>
-      
+
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
+
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
+
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          {offspring.map((hog)=>{
+            return <BabyHog key={hog.id} name={hog.name} hobby={hog.hobby} eyeColor={this.state.eyeColor}/>
+          })}
         </ul>
 
       </div>
