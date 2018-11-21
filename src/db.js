971b2e2233852713cb8457eb3803a8bcb5f2bcc2
srscalia <shelby.scalia@gmail.com>
2018-11-21 09:51:35 -0500

    Done.

--- src/db.js
+++ src/db.js
@@ -1,15 +1,15 @@
 var offspring = [
-  { 
+  {
     "id": 1,
     "name": "JeffKatzy Katz",
     "hobby": "Never gonna give"
   },
-  { 
+  {
     "id": 2,
     "name": "Rachel",
     "hobby": "You up, never gonna"
   },
-  { 
+  {
     "id": 3,
     "name": "Maxwell",
     "hobby": "let you down"
