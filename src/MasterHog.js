a5029d8f923344ab251e7d1a30fcb0574f21c5c8
free2580 <fridakahsas@yahoo.com>
2018-12-12 08:50:51 -0500

    Done.

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
+
+let masterOffspring = offspring
 
 export default class MasterHog extends Component {
 
@@ -19,6 +21,18 @@ export default class MasterHog extends Component {
     })
   }
 
+  mapBabyHogs = () => {
+  return masterOffspring.map(hog => {
+    return <BabyHog
+      eyeColor={this.state.eyeColor}
+      name={hog.name}
+      hobby={hog.hobby}
+      id={hog.id}
+      key={hog.id}
+    />
+    })
+  }
+
 
   render() {
     return (
@@ -30,19 +44,17 @@ export default class MasterHog extends Component {
         Sun<br></br>
         <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
         Glowing<br></br>
-      
+
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
+
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
+
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          {this.mapBabyHogs()}
         </ul>
 
       </div>
